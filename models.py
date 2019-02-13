16abeb2fc22d971ad27509646513431fe72f4326
Alberto Pinardi <pinardi.alberto@gmail.com>
2019-02-13 23:18:46 +0100

    Hack for formulari.cod display purpose

--- models.py
+++ models.py
@@ -71,9 +71,8 @@ class Formulari(models.Model):
 	stato = models.CharField(max_length=2, choices=SCELTA_STATO, default=APERTO,)
 	riepi = models.ForeignKey('formulari.Riepiloghi', models.SET_NULL, blank=True, null=True,)
 
-
         def __str__(self):
-                return self.cod
+                return self.cod.replace(" ","")
 
 	def importo(self):
 		return "%s \u20ac" %(self.prez * self.qu)
